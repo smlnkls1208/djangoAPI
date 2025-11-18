0968de7af516e591faec89ae1e8377bdaa72d94d
smlnkls1208 <al89994950059@gmail.com>
2025-11-18 17:45:00 +0700

    настройка токена

--- ebook_library/urls.py
+++ ebook_library/urls.py
@@ -2,10 +2,12 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from rest_framework.authtoken import views as token_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('library.urls')),
+    path('api/token/', token_views.obtain_auth_token),
 ]
 
 if settings.DEBUG:
--- library/apps.py
+++ library/apps.py
@@ -1,6 +1,16 @@
 from django.apps import AppConfig
 
-
 class LibraryConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'library'
+
+    def ready(self):
+        from django.db.models.signals import post_save
+        from django.dispatch import receiver
+        from django.conf import settings
+        from rest_framework.authtoken.models import Token
+
+        @receiver(post_save, sender=settings.AUTH_USER_MODEL)
+        def create_auth_token(sender, instance=None, created=False, **kwargs):
+            if created:
+                Token.objects.create(user=instance)
